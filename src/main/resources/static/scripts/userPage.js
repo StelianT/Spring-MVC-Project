6a1ae9468c6d65f4c9fe73b2d2dca184ad26d180
Steliyan Tokmakchiev <stelt3@gmail.com>
2017-05-01 10:34:02 +0300

    Added navigation to user pages

---
+++ src/main/resources/static/scripts/userPage.js
@@ -0,0 +1,19 @@
+function showQuotes() {
+    $('#picturesButton').removeClass('active');
+    $('#pictures').css('display', 'none');
+    $('#quotesButton').addClass('active');
+    $('#quotes').css('display', 'block');
+}
+
+function showPictures() {
+    $('#quotesButton').removeClass('active');
+    $('#quotes').css('display', 'none');
+    $('#picturesButton').addClass('active');
+    $('#pictures').css('display', 'block');
+}
+
+function showStories() {
+}
+
+function showMovies() {
+}
